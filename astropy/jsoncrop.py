7501b92a645e280e37b41da222d3a0cd45df1050
Astro_h <yhw_cx@163.com>
2022-06-13 23:15:04 +0800

    遥感图像和标注crop，修改了一些错误

--- astropy/jsoncrop.py
+++ astropy/jsoncrop.py
@@ -8,7 +8,9 @@
    2022.6.7 解决坐标转换
             用deepcopy解决变量赋值改变问题
             成功
-
+   2022.6.13 改了一个bug, 第87行,改成jsonpath
+             坐标参考系转换那里不支持polygons
+             改bug ，imagpath i j写反，已修改
 """
 
 from PIL import Image
@@ -20,29 +22,29 @@ import copy
 '''切割参数设置'''
 x = 0
 y = 0
-w = 1280
-h = 720
+w = 1024
+h = 1024
 overlap = 100
 stride1 = w-overlap
 stride2 = h-overlap
 #####################################################
 '''outputcrop的输出文件位置设置'''
-outputcropdir = r"C:\Users\Astro_h\Desktop\yanzheng\jsonyanzheng\outputcrop"
+outputcropdir = r"G:\datasets\hadatu\dataset\data1.0\addoutputcrop"
 if not os.path.exists(outputcropdir):
     os.makedirs(outputcropdir)
 
 '''jsoncrop的输出文件位置设置'''
-outjsoncropdir = r"C:\Users\Astro_h\Desktop\yanzheng\jsonyanzheng\outjsoncrop"
+outjsoncropdir = r"G:\datasets\hadatu\dataset\data1.0\addoutjsoncrop"
 if not os.path.exists(outjsoncropdir):
     os.makedirs(outjsoncropdir)
 #####################################################
 
-img_path =r"C:\Users\Astro_h\Desktop\yanzheng\jsonyanzheng\img"
+img_path =r"G:\datasets\hadatu\dataset\data1.0\addimg"
 fileList = os.listdir(img_path)
 print("fileList:", fileList)
 print('图片数量：', len(fileList))
 
-json_path = r"C:\Users\Astro_h\Desktop\yanzheng\jsonyanzheng\json"
+json_path = r"G:\datasets\hadatu\dataset\data1.0\addjson"
 json_list = os.listdir(json_path)
 print("json_list:", json_list)  # json名称的列表
 
@@ -84,9 +86,9 @@ for q in fileList:
     print(imgcrop)   #储存边界的二维列表
 
 ###################################################################################
-    json_path = os.path.join(json_path , (img_name+'.json'))
-    print(json_path)
-    with open(json_path, "r") as f:
+    jsonpath = os.path.join(json_path , (img_name+'.json'))
+    print(jsonpath)
+    with open(jsonpath, "r") as f:
          ann = json.load(f)
     #print(str(i)+'中的数据',ann['shapes'])
 
@@ -96,10 +98,14 @@ for q in fileList:
         print(i)
         # print(ann['shapes'][i])
         # print(ann['shapes'][i]['points'])
-        asp_x1 = min(ann['shapes'][i]['points'][0][0],ann['shapes'][i]['points'][1][0])
-        asp_y1 = min(ann['shapes'][i]['points'][0][1],ann['shapes'][i]['points'][1][1])
-        asp_x2 = max(ann['shapes'][i]['points'][1][0],ann['shapes'][i]['points'][1][0])
-        asp_y2 = max(ann['shapes'][i]['points'][1][1],ann['shapes'][i]['points'][0][1])
+        # asp_x1 = min(ann['shapes'][i]['points'][0][0],ann['shapes'][i]['points'][1][0])
+        asp_x1 = min(x[0] for x in ann['shapes'][i]['points'])   # 支持了polygons
+        # asp_y1 = min(ann['shapes'][i]['points'][0][1],ann['shapes'][i]['points'][1][1])
+        asp_y1 = min(x[1] for x in ann['shapes'][i]['points'])
+        # asp_x2 = max(ann['shapes'][i]['points'][1][0],ann['shapes'][i]['points'][1][0])
+        asp_x2 = max(x[0] for x in ann['shapes'][i]['points'])
+        # asp_y2 = max(ann['shapes'][i]['points'][1][1],ann['shapes'][i]['points'][0][1])
+        asp_y2 = max(x[1] for x in ann['shapes'][i]['points'])
         centerx=(asp_x1+asp_x2)/2
         centery=(asp_y1+asp_y2)/2
         print('中心点(',centerx,centery,')')
@@ -113,29 +119,34 @@ for q in fileList:
                 jscrop[croph][cropl]=copy.deepcopy([ann['shapes'][i]])  # 不能直接赋值，要deepcopy一份
             else:
                 jscrop[croph][cropl].append(copy.deepcopy(ann['shapes'][i]))
-        if centerx<imgcrop[croph-1][cropl-1][2] and centery<imgcrop[croph-1][cropl-1][3]:
+        # 判断左上角
+        if centerx<imgcrop[croph-1][cropl-1][2] and centery<imgcrop[croph-1][cropl-1][3]:   # 6.13发现，当croph=0时，croph-1=-1,列表加入错误，加到了jscrop[-1]
             if asp_x2<imgcrop[croph-1][cropl-1][2] and asp_y2<imgcrop[croph-1][cropl-1][3]:
-                print("true1")
-                if jscrop[croph-1][cropl-1] == 0:
-                    jscrop[croph-1][cropl-1] = copy.deepcopy([ann['shapes'][i]])
-                else:
-                    jscrop[croph-1][cropl-1].append(copy.deepcopy(ann['shapes'][i]))
 
+                if croph - 1 >= 0 and cropl - 1 >= 0:  # 6.13 加入判断，不能小于0
+                    print("true1")
+                    if jscrop[croph-1][cropl-1] == 0:
+                        jscrop[croph-1][cropl-1] = copy.deepcopy([ann['shapes'][i]])
+                    else:
+                        jscrop[croph-1][cropl-1].append(copy.deepcopy(ann['shapes'][i]))
+        # 判断正上方
         if centery<imgcrop[croph-1][cropl][3]:
             if asp_x1>imgcrop[croph-1][cropl][0] and asp_y1>imgcrop[croph-1][cropl][1] and asp_x2<imgcrop[croph-1][cropl][2] and asp_y2<imgcrop[croph-1][cropl][3]:
-                print("true2")
-                if jscrop[croph-1][cropl] == 0:
-                    jscrop[croph-1][cropl] = copy.deepcopy([ann['shapes'][i]])
-                else:
-                    jscrop[croph-1][cropl].append(copy.deepcopy(ann['shapes'][i]))
-
+                if croph - 1 >= 0 :  # 6.13 加入判断，不能小于0
+                    print("true2")
+                    if jscrop[croph-1][cropl] == 0:
+                        jscrop[croph-1][cropl] = copy.deepcopy([ann['shapes'][i]])
+                    else:
+                        jscrop[croph-1][cropl].append(copy.deepcopy(ann['shapes'][i]))
+        # 判断左方
         if centerx<imgcrop[croph][cropl-1][2]:
             if asp_x1 > imgcrop[croph][cropl-1][0] and asp_y1 > imgcrop[croph][cropl-1][1] and asp_x2 < imgcrop[croph][cropl-1][2] and asp_y2 < imgcrop[croph][cropl-1][3]:
-                print("true3")
-                if jscrop[croph][cropl-1] == 0:
-                    jscrop[croph][cropl-1] = copy.deepcopy([ann['shapes'][i]])
-                else:
-                    jscrop[croph][cropl-1].append(copy.deepcopy(ann['shapes'][i]))
+                if cropl - 1 >= 0:  # 6.13 加入判断，不能小于0
+                    print("true3")
+                    if jscrop[croph][cropl-1] == 0:
+                        jscrop[croph][cropl-1] = copy.deepcopy([ann['shapes'][i]])
+                    else:
+                        jscrop[croph][cropl-1].append(copy.deepcopy(ann['shapes'][i]))
         #################################################################
     num=0  # 有目标的json切片数量
     for i in range(len(jscrop)):  # i   循环  jscrop的行数
@@ -147,10 +158,22 @@ for q in fileList:
                 #########################################################
                 #坐标转换
                 for s in range(len(jscrop[i][j])):
-                    jscrop[i][j][s]["points"][0][0]-=j*stride1
-                    jscrop[i][j][s]["points"][0][1]-=i*stride2
+                    # 矩形框的标注
+                    jscrop[i][j][s]["points"][0][0]-=j*stride1  # 左上x # 6.13发现 这里不支持polygons
+                    jscrop[i][j][s]["points"][0][1]-=i*stride2  # 左上y
                     jscrop[i][j][s]["points"][1][0]-=j*stride1
                     jscrop[i][j][s]["points"][1][1]-=i*stride2
+                    ##############
+                    # 支持polygons的标注（进行中）
+                    # x_min = min(x[0] for x in jscrop[i][j][s]['points'])
+                    # x_min -= j * stride1   #左上 # 用另一个变量指向这个要改变的值，然后改这个变量  # 支持了polygons
+                    # y_min = min(x[1] for x in jscrop[i][j][s]['points'])
+                    # y_min -= i * stride2
+                    # x_max = max(x[0] for x in jscrop[i][j][s]['points'])
+                    # x_max -= j * stride1
+                    # y_max = max(x[1] for x in jscrop[i][j][s]['points'])
+                    # y_max -= i * stride
+                    ##############
                 print('转换后的jsoncrop',i,'行',j,'列',jscrop[i][j])
                 #########################################################
                 jsonname = str(img_name)+ 'crop_' + str(i) + str(j) + '.json'
@@ -160,7 +183,7 @@ for q in fileList:
                 c["version"]="4.6.0"
                 c["flags"]={}
                 c["shapes"]=jscrop[i][j]
-                c["imagePath"]=str(img_name) + 'crop_' + str(j) + str(i) + ".jpg"
+                c["imagePath"]=str(img_name) + 'crop_' + str(i) + str(j) + ".jpg"  # 2022.6.13 发现i和j反了，已改正
                 c["imageHeight"]=h
                 c["imageWidth"]=w
 
